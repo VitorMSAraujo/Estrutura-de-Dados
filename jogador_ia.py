efb2ec0df82b65ac37869e26150ca64edac33ab6
VitorMSAraujo <vitor18manuelz@gmail.com>
2024-12-17 21:50:22 -0300

    Fim da atividade

--- jogador_ia.py
+++ jogador_ia.py
@@ -1,23 +1,85 @@
-# -*- coding: utf-8 -*-
-from random import randint
-
+from random import choice
 from jogador import Jogador
 from tabuleiro import Tabuleiro
 
 class JogadorIA(Jogador):
-    def __init__(self, tabuleiro : Tabuleiro, tipo : int):
+    def __init__(self, tabuleiro: Tabuleiro, tipo: int):
         super().__init__(tabuleiro, tipo)
-            
 
     def getJogada(self) -> (int, int):
-        lista = []
-        for l in range(0,3):
-            for c in range(0,3):
-                if self.matriz[l][c] == Tabuleiro.DESCONHECIDO:
-                    lista.append((l, c))
-                    
-        if(len(lista) > 0):
-            p = randint(0, len(lista)-1)
-            return lista[p]
-        else:
-           return None
+        def verifica_vitoria_ou_bloqueio(tipo, alvo):
+            #Verifica linhas, colunas e diagonais para uma possível vitória ou bloqueio.
+            for i in range(3):
+                if sum(self.matriz[i][j] for j in range(3)) == alvo:  #Linha
+                    for j in range(3):
+                        if self.matriz[i][j] == Tabuleiro.DESCONHECIDO:
+                            return (i, j)
+                if sum(self.matriz[j][i] for j in range(3)) == alvo:  #Coluna
+                    for j in range(3):
+                        if self.matriz[j][i] == Tabuleiro.DESCONHECIDO:
+                            return (j, i)
+
+            # Diagonais
+            if sum(self.matriz[j][j] for j in range(3)) == alvo:
+                for j in range(3):
+                    if self.matriz[j][j] == Tabuleiro.DESCONHECIDO:
+                        return (j, j)
+            if sum(self.matriz[j][2 - j] for j in range(3)) == alvo:
+                for j in range(3):
+                    if self.matriz[j][2 - j] == Tabuleiro.DESCONHECIDO:
+                        return (j, 2 - j)
+            return None
+
+        def cria_duas_sequencias(tipo):
+            #Identifica uma jogada que cria duas sequências de duas marcações.
+            for l in range(3):
+                for c in range(3):
+                    if self.matriz[l][c] == Tabuleiro.DESCONHECIDO:
+                        # Simula a jogada
+                        self.matriz[l][c] = tipo
+                        count = 0
+                        if verifica_vitoria_ou_bloqueio(tipo, 2 * tipo):
+                            count += 1
+                        # Restaura o tabuleiro
+                        self.matriz[l][c] = Tabuleiro.DESCONHECIDO
+                        if count >= 2:  # Se criar duas sequências, retorna a jogada
+                            return (l, c)
+            return None
+
+        #Regra 1: Vitória ou bloqueio
+        jogada = verifica_vitoria_ou_bloqueio(self.tipo, 2 * self.tipo)  #Tentar vencer
+        if jogada:
+            return jogada
+
+        oponente = Tabuleiro.JOGADOR_0 if self.tipo == Tabuleiro.JOGADOR_X else Tabuleiro.JOGADOR_X
+        jogada = verifica_vitoria_ou_bloqueio(oponente, 2 * oponente)  #Bloquear oponente
+        if jogada:
+            return jogada
+
+        #Regra 2: Criar duas sequências
+        jogada = cria_duas_sequencias(self.tipo)
+        if jogada:
+            return jogada
+
+        #Regra 3: Marcar o centro
+        if self.matriz[1][1] == Tabuleiro.DESCONHECIDO:
+            return (1, 1)
+
+        #Regra 4: Marcar canto oposto
+        cantos_opostos = [((0, 0), (2, 2)), ((0, 2), (2, 0)), ((2, 0), (0, 2)), ((2, 2), (0, 0)),]
+        for canto, oposto in cantos_opostos:
+            if self.matriz[canto[0]][canto[1]] == oponente and self.matriz[oposto[0]][oposto[1]] == Tabuleiro.DESCONHECIDO:
+                return oposto
+
+        #Regra 5: Marcar qualquer canto vazio
+        cantos = [(0, 0), (0, 2), (2, 0), (2, 2)]
+        cantos_disponiveis = [canto for canto in cantos if self.matriz[canto[0]][canto[1]] == Tabuleiro.DESCONHECIDO]
+        if cantos_disponiveis:
+            return choice(cantos_disponiveis)
+
+        #Regra 6: Marcar qualquer posição vazia
+        disponiveis = [(l, c) for l in range(3) for c in range(3) if self.matriz[l][c] == Tabuleiro.DESCONHECIDO]
+        if disponiveis:
+            return choice(disponiveis)
+
+        return None
--- tabuleiro.py
+++ tabuleiro.py
@@ -6,13 +6,33 @@ class Tabuleiro:
     JOGADOR_X = 4
 
     def __init__(self):
-        self.matriz = [ [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO], 
-                        [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO],
-                        [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO]]
-        
-        #OI, SOU VITOR
+        # Inicializa o tabuleiro 3x3 com todas as posições desconhecidas
+        self.matriz = [[Tabuleiro.DESCONHECIDO] * 3 for _ in range(3)]
 
-       
-        
     def tem_campeao(self):
+        #Verifica se há um vencedor no tabuleiro.
+        
+        #Retorna:
+            #- JOGADOR_0 (1) se o jogador 0 venceu
+            #- JOGADOR_X (4) se o jogador X venceu
+            #- DESCONHECIDO (0) se não há vencedor
+
+        # Verifica linhas, colunas e diagonais
+        for i in range(3):
+            # Linhas e colunas
+            soma_linha = sum(self.matriz[i])
+            soma_coluna = sum(self.matriz[j][i] for j in range(3))
+
+            # Diagonal principal e secundária
+            soma_diag_principal = sum(self.matriz[j][j] for j in range(3))
+            soma_diag_secundaria = sum(self.matriz[j][2 - j] for j in range(3))
+
+            # Checa as somas para determinar o vencedor
+            for soma in [soma_linha, soma_coluna, soma_diag_principal, soma_diag_secundaria]:
+                if soma == 3:  # Três células do JOGADOR_0
+                    return Tabuleiro.JOGADOR_0
+                elif soma == 12:  # Três células do JOGADOR_X
+                    return Tabuleiro.JOGADOR_X
+
+        # Nenhum vencedor encontrado
         return Tabuleiro.DESCONHECIDO
